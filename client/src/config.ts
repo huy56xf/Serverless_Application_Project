aac0f0120ca7b1ecb1f6a38d04c865731035896c
huy <quanghuy56xf@gmail.com>
2022-12-20 23:41:59 +0700

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'rxuffbfv1d';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-6q3yordgrveyp1y4.us.auth0.com',
+  clientId: 'Jy4X3mZRhmBQRuyzAYD6nOOeNskSAGdm',
+  callbackUrl: 'http://localhost:3000/callback'
+};
